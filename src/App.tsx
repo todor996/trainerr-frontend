a146bdd0489537002ca78f45bcf9c88ef3ed1f43
Pavle Marinkovic <iampalve.sync@gmail.com>
2023-08-17 18:30:50 +0200

    Add Redux Toolkit and basic store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { useAppSelector } from "./store/hooks.store";
 
 export function App(): JSX.Element {
+  const userName = useAppSelector((state) => state.home.userName);
+  const title = useAppSelector((state) => state.home.title);
+
   return (
-    <>
-      <h1 className="text-3xl font-bold my-4">Hello world!</h1>
+    <div className="flex flex-col justify-start p-4 items-center border border-solid border-yellow-600 rounded">
+      <h1 className="text-3xl font-medium">App.tsx</h1>
+      <h2 className="text-2xl font-bold my-4">{title}</h2>
+      <p className="my-1">Guest: {userName}</p>
       <RouterProvider router={router} />
-    </>
+    </div>
   );
 }
 
--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -1,8 +1,27 @@
+import { updateHomeState } from "../../store/home/home.slice";
+import { useAppDispatch, useAppSelector } from "../../store/hooks.store";
+
 // TODO: Create Snippet for React Functional Component
 export function Home(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const userName = useAppSelector((state) => state.home.userName);
+
+  function updateUserName(userName: string): void {
+    dispatch(updateHomeState({ userName }));
+  }
   return (
-    <p>
-      Trainerr is showing you <b>Home Page</b>
-    </p>
+    <div className="flex-col p-4 border border-solid border-yellow-600 rounded">
+      <h2 className="text-2xl my-2 font-medium">Home.page.tsx</h2>
+      <label className="flex flex-col max-w-xs">
+        <span>Guest</span>
+        <input
+          className="border border-solid border-gray-600 rounded p-1"
+          type="text"
+          placeholder="Username"
+          value={userName}
+          onChange={(event) => updateUserName(event.target.value)}
+        />
+      </label>
+    </div>
   );
 }
