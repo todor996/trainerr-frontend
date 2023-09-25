55a5b00928e258446d226cc19375c80f07536786
todor996 <todor_1996@live.com>
2023-09-25 15:09:27 +0200

    Added storybook, pwa and shacdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  VitePWA({ registerType: 'autoUpdate' })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
