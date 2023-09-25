a12b016b55220011a7ca0499192a3d234f1a02b7
todor996 <todor_1996@live.com>
2023-09-25 14:30:50 +0200

    Added i18n

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
 import { useAppSelector } from "./store/hooks.store";
+import './localization/i18n';
+import { useTranslation } from "react-i18next";
 
 export function App(): JSX.Element {
+    const { t } = useTranslation();
   const userName = useAppSelector((state) => state.home.userName);
   const title = useAppSelector((state) => state.home.title);
 
@@ -11,7 +14,9 @@ export function App(): JSX.Element {
     <div className="flex flex-col justify-start p-4 items-center border border-solid border-yellow-600 rounded">
       <h1 className="text-3xl font-medium">App.tsx</h1>
       <h2 className="text-2xl font-bold my-4">{title}</h2>
-      <p className="my-1">Guest: {userName}</p>
+        <h3 className="text-2xl font-bold my-4">{t('title', { name: 'user'})}</h3>
+
+        <p className="my-1">Guest: {userName}</p>
       <RouterProvider router={router} />
     </div>
   );
