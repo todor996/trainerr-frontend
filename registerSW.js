8e34d179e380f4e9ac0f7235a80f575a71b9b020
todor996 <todor_1996@live.com>
2023-12-24 00:22:35 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/trainer-frontend/sw.js', { scope: '/trainer-frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
