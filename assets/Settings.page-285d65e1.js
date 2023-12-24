d8fad3a9fe89b73c3d310589c5a0ed1fc65b0b76
todor996 <todor_1996@live.com>
2023-12-24 01:00:08 +0100

    Updates

---
+++ assets/Settings.page-285d65e1.js
@@ -0,0 +1,1 @@
+import{j as s,L as e}from"./index-ccace048.js";function i(){return s.jsxs("div",{className:"flex flex-col",children:["Hello from ",s.jsx("b",{children:"Settings Page!"}),s.jsxs("div",{className:"join",children:[s.jsx(e,{className:"btn join-item",to:"/",children:"Home"}),s.jsx(e,{className:"btn join-item",to:"/settings",children:"Settings"})]})]})}export{i as default};
