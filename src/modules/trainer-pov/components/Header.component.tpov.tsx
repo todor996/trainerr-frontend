2a7b4eac181cceb25179dbe9b3c0037afbd498be
Pavle Marinkovic <iampavle.sync@gmail.com>
2024-01-27 21:56:38 +0100

    Remove unneeded Dropdown code from Header

--- src/modules/trainer-pov/components/Header.component.tpov.tsx
+++ src/modules/trainer-pov/components/Header.component.tpov.tsx
@@ -1,10 +1,8 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Dropdown, Theme } from 'react-daisyui';
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { Theme } from 'react-daisyui';
 
 import { Button } from '@shared/components/Button/Button.component';
-import { Icon } from '@shared/components/Icon.component';
 import { localKeys } from '@shared/consts/localization.const';
 import { TrrDropdown } from '@shared/components/TrrDropdown.component';
 import { ThemeItem } from '@shared/components/ThemeItem.component';
@@ -12,43 +10,19 @@ import { DEFAULT_THEMES } from '@shared/consts/daisyui.const';
 
 export function Header(): JSX.Element {
   const { t, i18n } = useTranslation();
-  const dropdownRef = useRef<HTMLDivElement>(null);
 
-  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
-  const [currentTheme, setCurrentTheme] = useState(getThemeDOM());
-
-  // TODO: Think about making custom hook for handling outside click events
-  useEffect(() => {
-    document.addEventListener('click', handleOutsideDropdownClick, { capture: true });
-
-    return () => {
-      document.removeEventListener('click', handleOutsideDropdownClick);
-    };
-  }, []);
-
-  function handleOutsideDropdownClick(event: MouseEvent): void {
-    if (dropdownRef.current?.contains(event.target as Node)) {
-      return;
-    }
-
-    setIsDropdownOpened(false);
-  }
+  const [currentTheme, setCurrentTheme] = useState(getThemeDom());
 
   function setLanguage(key: string): void {
     i18n.changeLanguage(key);
   }
 
-  function toggleDropdown() {
-    setIsDropdownOpened(!isDropdownOpened);
-  }
-
-  function getThemeDOM(): string {
+  function getThemeDom(): string {
     return document.querySelector('html')?.dataset.theme as string;
   }
 
   function setThemeDom(theme: string): void {
     document.querySelector('html')!.dataset.theme = theme;
-    setCurrentTheme(theme);
   }
 
   return (
@@ -85,7 +59,10 @@ export function Header(): JSX.Element {
               </Theme>
             );
           }}
-          onSelect={(theme) => setThemeDom(theme as string)}
+          onSelect={(theme) => {
+            setThemeDom(theme as string);
+            setCurrentTheme(theme as string);
+          }}
         />
 
         <TrrDropdown
@@ -98,39 +75,6 @@ export function Header(): JSX.Element {
           }))}
           onSelect={(key) => setLanguage(key as string)}
         />
-
-        {/* TODO: Remove this component and all unneeded logic for it */}
-        {false && (
-          <Dropdown
-            end={true}
-            open={isDropdownOpened}
-            key={isDropdownOpened ? 'true' : 'false'}
-            ref={dropdownRef}
-          >
-            <Dropdown.Toggle button={false} size="sm" onClick={toggleDropdown}>
-              <Button className="btn-outline btn-sm" data-toggle>
-                {t(`${i18n.language}.full`)}{' '}
-                <Icon
-                  className={isDropdownOpened ? 'rotate-180' : 'rotate-0'}
-                  size={8}
-                  icon={faChevronDown}
-                />
-              </Button>
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu className="mt-1">
-              {localKeys.map((key, index) => (
-                <Button
-                  className={`btn-ghost btn-sm mt-1 items-center ${key === i18n.language ? 'btn-active' : ''}`}
-                  key={index}
-                  onClick={() => setLanguage(key)}
-                >
-                  {t(`${key}.full`)}
-                </Button>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        )}
       </div>
     </div>
   );
