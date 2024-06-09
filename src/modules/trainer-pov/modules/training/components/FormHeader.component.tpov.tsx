d8a796f68b012cc5f11349cbc976a88a8841fd66
Pavle Marinkovic <iampavle.sync@gmail.com>
2024-06-09 19:12:30 +0200

    Remove redux packages

--- src/modules/trainer-pov/modules/training/components/FormHeader.component.tpov.tsx
+++ src/modules/trainer-pov/modules/training/components/FormHeader.component.tpov.tsx
@@ -1,8 +1,9 @@
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { TrrIcon } from '@shared/components/TrrIcon.component';
-import { SizableText, XStack, styled } from 'tamagui';
+import { SizableText, TamaguiElement, XStack, styled } from 'tamagui';
 import { useNavigate } from 'react-router-dom';
 import { TrrButton } from '@shared/components/TrrButton.component';
+import { LegacyRef, forwardRef } from 'react';
 
 interface FormHeaderProps {
   className?: string;
@@ -10,41 +11,44 @@ interface FormHeaderProps {
   children?: React.ReactNode;
 }
 
-function FormHeaderUnstyled(props: FormHeaderProps): JSX.Element {
-  const { text, children, ...otherProps } = props;
-  const navigate = useNavigate();
+const FormHeaderUnstyled = forwardRef(
+  (props: FormHeaderProps, ref: LegacyRef<TamaguiElement>) => {
+    const { text, children, ...otherProps } = props;
+    const navigate = useNavigate();
 
-  function goBack() {
-    navigate(-1);
-  }
+    function goBack() {
+      navigate(-1);
+    }
 
-  return (
-    <XStack
-      width="100%"
-      alignItems="center"
-      justifyContent="space-between"
-      backgroundColor="$base-100"
-      paddingHorizontal="24px"
-      paddingVertical="12px"
-      {...otherProps}
-    >
-      {/* LEFT */}
-      <XStack marginLeft="-2px" alignItems="center">
-        <TrrButton size="$3" onPress={goBack}>
-          <TrrIcon icon={faChevronLeft} size={16} />
-        </TrrButton>
-        <SizableText fontSize="20px" fontWeight="500">
-          {text}
-        </SizableText>
+    return (
+      <XStack
+        ref={ref}
+        width="100%"
+        alignItems="center"
+        justifyContent="space-between"
+        backgroundColor="$base-100"
+        paddingHorizontal="24px"
+        paddingVertical="12px"
+        {...otherProps}
+      >
+        {/* LEFT */}
+        <XStack marginLeft="-2px" alignItems="center">
+          <TrrButton size="$3" onPress={goBack}>
+            <TrrIcon icon={faChevronLeft} size={16} />
+          </TrrButton>
+          <SizableText fontSize="20px" fontWeight="500">
+            {text}
+          </SizableText>
+        </XStack>
+
+        {/* TODO: Think of connecting PlanForm - <form> and this <form>  */}
+        {/* RIGHT */}
+        <XStack className="items-center-justify-end flex flex-row space-x-2">
+          {children}
+        </XStack>
       </XStack>
-
-      {/* TODO: Think of connecting PlanForm - <form> and this <form>  */}
-      {/* RIGHT */}
-      <XStack className="items-center-justify-end flex flex-row space-x-2">
-        {children}
-      </XStack>
-    </XStack>
-  );
-}
+    );
+  },
+);
 
 export const FormHeader = styled(FormHeaderUnstyled, {});
